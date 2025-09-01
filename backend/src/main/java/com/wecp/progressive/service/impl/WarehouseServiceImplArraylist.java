145cee02d18ada2943f0b232a392041512dad61f
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 14:30:14 +0530

    Update WarehouseServiceImplArraylist.java

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
@@ -1,37 +1,29 @@
 package com.wecp.progressive.service.impl;
 
-import com.wecp.progressive.entity.Supplier;
 import com.wecp.progressive.entity.Warehouse;
 import com.wecp.progressive.service.WarehouseService;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class WarehouseServiceImplArraylist implements WarehouseService {
 
-    private static List<Warehouse> warehouseList = new ArrayList<>();
-
     @Override
     public List<Warehouse> getAllWarehouses() {
-        return warehouseList;
+        return List.of();
     }
 
     @Override
     public int addWarehouse(Warehouse warehouse) {
-        warehouseList.add(warehouse);
-        return warehouseList.size();
+        return -1;
     }
 
     @Override
     public List<Warehouse> getWarehousesSortedByCapacity() {
-        List<Warehouse> sortedWarehouses = warehouseList;
-        sortedWarehouses.sort(Comparator.comparing(Warehouse::getCapacity)); // Sort by supplier name
-        return sortedWarehouses;
+        return List.of();
     }
 
     @Override
     public void emptyArrayList() {
-        warehouseList = new ArrayList<>();
+        WarehouseService.super.emptyArrayList();
     }
 }
