6b01e59a6ded2128f4838750de26ae23d83f6208
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 14:54:07 +0530

    Update SupplierServiceImplJdbc.java

--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJdbc.java
@@ -4,6 +4,8 @@ import com.wecp.progressive.dao.SupplierDAO;
 import com.wecp.progressive.entity.Supplier;
 import com.wecp.progressive.service.SupplierService;
 
+import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.List;
 
 public class SupplierServiceImplJdbc implements SupplierService {
@@ -15,31 +17,36 @@ public class SupplierServiceImplJdbc implements SupplierService {
     }
 
     @Override
-    public List<Supplier> getAllSuppliers() {
-        return List.of();
+    public List<Supplier> getAllSuppliers() throws SQLException {
+        return supplierDAO.getAllSuppliers();
     }
 
     @Override
-    public int addSupplier(Supplier supplier) {
-        return -1;
+    public int addSupplier(Supplier supplier) throws SQLException {
+        return supplierDAO.addSupplier(supplier);
     }
 
     @Override
-    public List<Supplier> getAllSuppliersSortedByName() {
-        return List.of();
+    public List<Supplier> getAllSuppliersSortedByName() throws SQLException {
+        List<Supplier> sortedSuppliers = supplierDAO.getAllSuppliers();
+        if (sortedSuppliers != null) {
+            sortedSuppliers.sort(Comparator.comparing(Supplier::getSupplierName)); // Sort by supplier name
+        }
+        return sortedSuppliers;
     }
 
     @Override
-    public void updateSupplier(Supplier supplier) {
+    public void updateSupplier(Supplier supplier) throws SQLException {
+        supplierDAO.updateSupplier(supplier);
     }
 
     @Override
-    public void deleteSupplier(int supplierId) {
-
+    public void deleteSupplier(int supplierId) throws SQLException {
+        supplierDAO.deleteSupplier(supplierId);
     }
 
     @Override
-    public Supplier getSupplierById(int supplierId) {
-        return null;
+    public Supplier getSupplierById(int supplierId) throws SQLException {
+        return supplierDAO.getSupplierById(supplierId);
     }
 }
