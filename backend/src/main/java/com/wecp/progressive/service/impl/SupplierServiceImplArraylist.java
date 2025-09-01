7e1b788512dac6447367f787dc129bcecf26c83e
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:44:01 +0530

    Update SupplierServiceImplArraylist.java

--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplArraylist.java
@@ -1,5 +1,29 @@
 package com.wecp.progressive.service.impl;
 
-public class SupplierServiceImplArraylist  {
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.service.SupplierService;
 
+import java.util.List;
+
+public class SupplierServiceImplArraylist implements SupplierService {
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return List.of();
+    }
+
+    @Override
+    public int addSupplier(Supplier supplier) {
+        return -1;
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliersSortedByName() {
+        return List.of();
+    }
+
+    @Override
+    public void emptyArrayList() {
+        SupplierService.super.emptyArrayList();
+    }
 }
