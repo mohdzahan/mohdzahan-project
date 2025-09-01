8182e86294dd20c423deb9b1c0a4f3eeda200d80
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:42:40 +0530

    Update ProductDAOImpl.java

--- backend/src/main/java/com/wecp/progressive/dao/ProductDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/ProductDAOImpl.java
@@ -1,5 +1,33 @@
 package com.wecp.progressive.dao;
 
-public class ProductDAOImpl {
+import com.wecp.progressive.entity.Product;
 
+import java.util.List;
+
+public class ProductDAOImpl implements ProductDAO {
+
+    @Override
+    public int addProduct(Product products) {
+        return -1;
+    }
+
+    @Override
+    public Product getProductById(int productId) {
+        return null;
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+
+    }
+
+    @Override
+    public void deleteProduct(int productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
 }
