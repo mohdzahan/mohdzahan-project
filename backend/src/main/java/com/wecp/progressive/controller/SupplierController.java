787b2d49368daa656e0ecb343cd9fc7c0c2ef025
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 14:58:02 +0530

    Update SupplierController.java

--- backend/src/main/java/com/wecp/progressive/controller/SupplierController.java
+++ backend/src/main/java/com/wecp/progressive/controller/SupplierController.java
@@ -2,9 +2,11 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Supplier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class SupplierController {
 
     public ResponseEntity<List<Supplier>> getAllSuppliers() {
@@ -19,7 +21,7 @@ public class SupplierController {
         return null;
     }
 
-    public ResponseEntity<Void> updateSupplier(Supplier supplier) {
+    public ResponseEntity<Void> updateSupplier(int supplierId, Supplier supplier) {
         return null;
     }
 
