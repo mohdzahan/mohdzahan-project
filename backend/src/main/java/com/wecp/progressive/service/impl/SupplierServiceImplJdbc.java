a3faea48d4c495d5843a41e29e8ef044b9fcea06
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:50:26 +0530

    Update SupplierServiceImplJdbc.java

--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJdbc.java
@@ -1,5 +1,45 @@
 package com.wecp.progressive.service.impl;
 
-public class SupplierServiceImplJdbc  {
+import com.wecp.progressive.dao.SupplierDAO;
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.service.SupplierService;
 
+import java.util.List;
+
+public class SupplierServiceImplJdbc implements SupplierService {
+
+    private SupplierDAO supplierDAO;
+
+    public SupplierServiceImplJdbc(SupplierDAO supplierDAO) {
+        this.supplierDAO = supplierDAO;
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return List.of();
+    }
+
+    @Override
+    public int addSupplier(Supplier supplier) {
+        return -1;
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliersSortedByName() {
+        return List.of();
+    }
+
+    @Override
+    public void updateSupplier(Supplier supplier) {
+    }
+
+    @Override
+    public void deleteSupplier(int supplierId) {
+
+    }
+
+    @Override
+    public Supplier getSupplierById(int supplierId) {
+        return null;
+    }
 }
