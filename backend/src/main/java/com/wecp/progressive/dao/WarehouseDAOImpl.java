c6348f8267201e3f39fa9b2aba74b58b3ca6aa02
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:48:05 +0530

    Update WarehouseDAOImpl.java

--- backend/src/main/java/com/wecp/progressive/dao/WarehouseDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/WarehouseDAOImpl.java
@@ -1,5 +1,33 @@
 package com.wecp.progressive.dao;
 
-public class WarehouseDAOImpl {
+import com.wecp.progressive.entity.Warehouse;
 
+import java.util.List;
+
+public class WarehouseDAOImpl implements WarehouseDAO {
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) {
+        return -1;
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId) {
+        return null;
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) {
+
+    }
+
+    @Override
+    public void deleteWarehouse(int warehouseId) {
+
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouse() {
+        return List.of();
+    }
 }
