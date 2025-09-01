a85b2dbbdb0fc9699313f500feb06f68171ad903
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:44:40 +0530

    Update ProductServiceImplJdbc.java

--- backend/src/main/java/com/wecp/progressive/service/impl/ProductServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/ProductServiceImplJdbc.java
@@ -1,5 +1,42 @@
 package com.wecp.progressive.service.impl;
 
-public class ProductServiceImplJdbc  {
+import com.wecp.progressive.dao.ProductDAO;
+import com.wecp.progressive.entity.Product;
+import com.wecp.progressive.service.ProductService;
+
+import java.util.List;
+
+public class ProductServiceImplJdbc implements ProductService {
+
+    private ProductDAO productDAO;
+
+    public ProductServiceImplJdbc(ProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product getProductById(int productId) {
+        return null;
+    }
+
+    @Override
+    public int addProduct(Product product) {
+        return -1;
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+
+    }
+
+    @Override
+    public void deleteProduct(int productId) {
+
+    }
 
 }
