8675c5b8d59c97b6f631d848998548409a085807
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:49:19 +0530

    Update WarehouseServiceImplJdbc.java

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJdbc.java
@@ -1,5 +1,52 @@
 package com.wecp.progressive.service.impl;
 
-public class WarehouseServiceImplJdbc  {
+import com.wecp.progressive.dao.WarehouseDAO;
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.entity.Warehouse;
+import com.wecp.progressive.service.WarehouseService;
 
+import java.util.List;
+
+public class WarehouseServiceImplJdbc implements WarehouseService {
+
+    private WarehouseDAO warehouseDAO;
+
+    public WarehouseServiceImplJdbc(WarehouseDAO warehouseDAO) {
+        this.warehouseDAO = warehouseDAO;
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouses() {
+        return List.of();
+    }
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) {
+        return -1;
+    }
+
+    @Override
+    public List<Warehouse> getWarehousesSortedByCapacity() {
+        return List.of();
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) {
+
+    }
+
+    @Override
+    public void deleteWarehouse(int warehouseId) {
+
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId) {
+        return null;
+    }
+
+    @Override
+    public List<Warehouse> getWarehouseBySupplier(int supplierId) {
+        return null;
+    }
 }
