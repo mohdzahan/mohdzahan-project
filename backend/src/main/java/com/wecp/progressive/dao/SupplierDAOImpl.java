88880486062fca0e38403b301b417b892d38a7bc
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 09:48:39 +0530

    Update SupplierDAOImpl.java

--- backend/src/main/java/com/wecp/progressive/dao/SupplierDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/SupplierDAOImpl.java
@@ -1,8 +1,35 @@
 package com.wecp.progressive.dao;
 
 
-public class SupplierDAOImpl {
+import com.wecp.progressive.entity.Supplier;
+
+import java.util.List;
+
+public class SupplierDAOImpl implements SupplierDAO {
 
 
+    @Override
+    public int addSupplier(Supplier supplier) {
+        return -1;
+    }
 
+    @Override
+    public Supplier getSupplierById(int supplierId) {
+        return null;
+    }
+
+    @Override
+    public void updateSupplier(Supplier supplier) {
+
+    }
+
+    @Override
+    public void deleteSupplier(int supplierId) {
+
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return List.of();
+    }
 }
