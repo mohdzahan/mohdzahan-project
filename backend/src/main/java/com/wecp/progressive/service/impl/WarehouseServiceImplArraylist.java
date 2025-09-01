da683d264ac8dc7524fd62e9abdfd562577abdfc
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-01 15:03:46 +0530

    Update WarehouseServiceImplArraylist.java

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
@@ -3,11 +3,13 @@ package com.wecp.progressive.service.impl;
 import com.wecp.progressive.entity.Supplier;
 import com.wecp.progressive.entity.Warehouse;
 import com.wecp.progressive.service.WarehouseService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+@Service
 public class WarehouseServiceImplArraylist implements WarehouseService {
 
     private static List<Warehouse> warehouseList = new ArrayList<>();
