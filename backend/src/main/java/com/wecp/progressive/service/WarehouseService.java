4b2a169a305128f2cdf3c8ace4a6588009ac282b
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:32:07 +0530

    Update WarehouseService.java

--- backend/src/main/java/com/wecp/progressive/service/WarehouseService.java
+++ backend/src/main/java/com/wecp/progressive/service/WarehouseService.java
@@ -5,16 +5,29 @@ import com.wecp.progressive.entity.Warehouse;
 import java.sql.SQLException;
 import java.util.List;
 
+
 public interface WarehouseService {
     List<Warehouse> getAllWarehouses() throws SQLException;
+
     int addWarehouse(Warehouse warehouse) throws SQLException;
+
     List<Warehouse> getWarehousesSortedByCapacity() throws SQLException;
-    default public void emptyArrayList() {}
-    default void updateWarehouse(Warehouse warehouse) throws SQLException {}
-		default void deleteWarehouse(int warehouseId) throws SQLException {}
-    default Warehouse getWarehouseById(int warehouseId) throws SQLException {
+
+    //Do not implement these methods in WarehouseServiceImplArraylist.java class
+
+    default void emptyArrayList(){
+        
+    }
+    default void updateWarehouse(Warehouse warehouse) throws SQLException{
+    }
+
+    default void deleteWarehouse(int warehouseId) throws SQLException{
+    }
+
+    default Warehouse getWarehouseById(int warehouseId) throws SQLException{
         return null;
     }
+    //Do not implement these methods in WarehouseServiceImplArraylist.java and WarehouseServiceImplJdbc.java class
     default List<Warehouse> getWarehouseBySupplier(int supplierId) {
         return null;
     }
