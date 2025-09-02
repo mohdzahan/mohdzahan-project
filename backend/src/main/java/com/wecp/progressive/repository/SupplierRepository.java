c4c5d65d1c075221c8592d79f4cf069848f1f7f5
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:30:53 +0530

    Update SupplierRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
@@ -1,5 +1,15 @@
 package com.wecp.progressive.repository;
 
 
-public interface SupplierRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.wecp.progressive.entity.Supplier;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
+
+    Supplier deleteBySupplierId(@Param("supplierId") int supplier_id);
+    Supplier findBySupplierId(@Param("supplierId") int supplierId);
+    
 }
