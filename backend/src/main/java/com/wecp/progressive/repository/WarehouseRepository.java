fe46509b54ef322bf4c5028f7c51773e9bc8391a
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:31:14 +0530

    Update WarehouseRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/WarehouseRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/WarehouseRepository.java
@@ -1,5 +1,8 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WarehouseRepository {
+import com.wecp.progressive.entity.Warehouse;
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, Integer>{
 }
