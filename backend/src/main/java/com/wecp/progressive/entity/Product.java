e9405bd689708105b80dc77642250990a33cf8d3
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:29:07 +0530

    Update Product.java

--- backend/src/main/java/com/wecp/progressive/entity/Product.java
+++ backend/src/main/java/com/wecp/progressive/entity/Product.java
@@ -15,11 +15,10 @@ public class Product {
     private String productDescription;
     private int quantity;
     private Long price;
-
     public Product() {
     }
-
-    public Product(int productId, int warehouseId, String productName, String productDescription, int quantity, Long price) {
+    public Product(int productId, int warehouseId, String productName, String productDescription, int quantity,
+            Long price) {
         this.productId = productId;
         this.warehouseId = warehouseId;
         this.productName = productName;
@@ -27,52 +26,41 @@ public class Product {
         this.quantity = quantity;
         this.price = price;
     }
-
     public int getProductId() {
         return productId;
     }
-
     public void setProductId(int productId) {
         this.productId = productId;
     }
-
     public int getWarehouseId() {
         return warehouseId;
     }
-
     public void setWarehouseId(int warehouseId) {
         this.warehouseId = warehouseId;
     }
-
     public String getProductName() {
         return productName;
     }
-
     public void setProductName(String productName) {
         this.productName = productName;
     }
-
     public String getProductDescription() {
         return productDescription;
     }
-
     public void setProductDescription(String productDescription) {
         this.productDescription = productDescription;
     }
-
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-
     public Long getPrice() {
         return price;
     }
-
     public void setPrice(Long price) {
         this.price = price;
     }
+    
 }
