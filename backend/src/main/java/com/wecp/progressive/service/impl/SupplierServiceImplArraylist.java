3687cd0a6dd4ab67b8c5cfba43f7df6e1d5c5af8
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:36:32 +0530

    Update SupplierServiceImplArraylist.java

--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplArraylist.java
@@ -1,38 +1,46 @@
 package com.wecp.progressive.service.impl;
 
-import com.wecp.progressive.entity.Supplier;
-import com.wecp.progressive.service.SupplierService;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
-import java.util.Comparator;
+
+import java.util.Collections;
+
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.service.SupplierService;
+
 @Service
-public class SupplierServiceImplArraylist implements SupplierService {
+public class SupplierServiceImplArraylist  implements SupplierService{
 
-    private static List<Supplier> supplierList = new ArrayList<>();
 
-    @Override
-    public List<Supplier> getAllSuppliers() {
-        return supplierList;
-    }
+    private static List<Supplier> suppliers = new ArrayList<>();
 
     @Override
     public int addSupplier(Supplier supplier) {
-        supplierList.add(supplier);
-        return supplierList.size();
+
+        suppliers.add(supplier);
+        return suppliers.size();
     }
 
+    @Override
+    public List<Supplier> getAllSuppliers() {
+
+        return suppliers;
+    }
     @Override
     public List<Supplier> getAllSuppliersSortedByName() {
-        List<Supplier> sortedSupplier = supplierList;
-        sortedSupplier.sort(Comparator.comparing(Supplier::getSupplierName)); // Sort by supplier name
-        return sortedSupplier;
-    }
 
+        List<Supplier> supplierSort = new ArrayList<>(suppliers);
+        Collections.sort(supplierSort);
+        return supplierSort;
+    }
     @Override
     public void emptyArrayList() {
-        supplierList = new ArrayList<>();
+
+        suppliers = new ArrayList<>();
+
     }
+
 }
