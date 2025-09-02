f13558ea7de4f7e802317625b4c4e6f5b571c49b
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:34:06 +0530

    Update WarehouseServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJpa.java
@@ -1,38 +1,39 @@
 package com.wecp.progressive.service.impl;
 
-import com.wecp.progressive.entity.Warehouse;
-import com.wecp.progressive.service.WarehouseService;
-import org.springframework.stereotype.Service;
-
 import java.sql.SQLException;
 import java.util.List;
 
-@Service
-public class WarehouseServiceImplJpa implements WarehouseService {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wecp.progressive.entity.Warehouse;
+import com.wecp.progressive.repository.WarehouseRepository;
+import com.wecp.progressive.service.WarehouseService;
+
+public class WarehouseServiceImplJpa  implements WarehouseService{
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) throws SQLException {
+        
+        return -1;
+    }
+public WarehouseServiceImplJpa(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+
+
+    @Override
+    public void deleteWarehouse(int warehouseId) throws SQLException {
+       
+    }
 
     @Override
     public List<Warehouse> getAllWarehouses() throws SQLException {
         return List.of();
-    }
-
-    @Override
-    public int addWarehouse(Warehouse warehouse) throws SQLException {
-        return -1;
-    }
-
-    @Override
-    public List<Warehouse> getWarehousesSortedByCapacity() throws SQLException {
-        return List.of();
-    }
-
-    @Override
-    public void updateWarehouse(Warehouse warehouse) throws SQLException {
-
-    }
-
-    @Override
-    public void deleteWarehouse(int warehouseId) throws SQLException {
-
     }
 
     @Override
@@ -44,4 +45,15 @@ public class WarehouseServiceImplJpa implements WarehouseService {
     public List<Warehouse> getWarehouseBySupplier(int supplierId) {
         return null;
     }
+
+    @Override
+    public List<Warehouse> getWarehousesSortedByCapacity() throws SQLException {
+       return List.of();
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) throws SQLException {
+        
+    }
+    
 }
