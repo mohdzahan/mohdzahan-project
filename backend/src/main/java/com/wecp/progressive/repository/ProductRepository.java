d38ca01f4339a95715813a0d034ea581e0099240
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-02 18:30:33 +0530

    Update ProductRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/ProductRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository {
+import com.wecp.progressive.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
 }
