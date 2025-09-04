ff0ef0c3bc6029fa2a351c3f81d5cae4209d614a
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-04 10:08:26 +0530

    Update SupplyLinkApplication.java

--- backend/src/main/java/com/wecp/progressive/SupplyLinkApplication.java
+++ backend/src/main/java/com/wecp/progressive/SupplyLinkApplication.java
@@ -1,8 +1,12 @@
 package com.wecp.progressive;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class SupplyLinkApplication {
     public static void main(String[] args) {
