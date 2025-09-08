c4d81a10beee8dbbe4716866628841b81a3c2b51
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-08 10:22:49 +0530

    Day 9.java

--- backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
+++ backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
@@ -1,35 +1,33 @@
 package com.wecp.progressive.config;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
 public class DatabaseConnectionManager {
     private static final Properties properties = new Properties();
-
-    static{
+    static {
+        
         loadProperties();
     }
-
-    private static void loadProperties(){
-    try (InputStream input = DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("application.properties")) {
-        if(input == null){
-            throw new IllegalStateException("resources.properties not found in classpath");
+    private static void loadProperties() {
+try (InputStream input = DatabaseConnectionManager.class.getClassLoader()
+.getResourceAsStream("application.properties")) 
+{
+            if (input == null) {
+                throw new IllegalStateException("resource.properties not found in classpath");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading properties file", e);
         }
-        properties.load(input);
-    } catch (Exception e) {
-        throw new RuntimeException("Error loading properties file", e);
     }
-    }
-
-    public static Connection getConnection() throws SQLException{
+    public static Connection getConnection() throws SQLException {
         String url = properties.getProperty("spring.datasource.url");
         String user = properties.getProperty("spring.datasource.username");
-        String password = properties.getProperty("spring.datasource.password");
-        return DriverManager.getConnection(url,user,password);
+     String password = properties.getProperty("spring.datasource.password");
+        return DriverManager.getConnection(url, user, password);
     }
-
 }
