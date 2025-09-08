a07e8812768c2feeb0ab2e0464a16af683595a7e
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-08 09:39:54 +0530

    Day 9.java

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJdbc.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-
 import com.wecp.progressive.dao.WarehouseDAO;
 import com.wecp.progressive.entity.Warehouse;
 import com.wecp.progressive.service.WarehouseService;
