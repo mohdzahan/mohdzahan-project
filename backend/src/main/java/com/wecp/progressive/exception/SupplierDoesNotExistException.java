ba232ac42b9533b27fa6a4659e430668ddbbca92
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-08 09:54:01 +0530

    Day 9.java

--- backend/src/main/java/com/wecp/progressive/exception/SupplierDoesNotExistException.java
+++ backend/src/main/java/com/wecp/progressive/exception/SupplierDoesNotExistException.java
@@ -1,7 +1,7 @@
 package com.wecp.progressive.exception;
 
-public class SupplierDoesNotExistException extends RuntimeException {
-    public SupplierDoesNotExistException(String message) {
-        super(message);
+public class SupplierDoesNotExistException extends RuntimeException{
+    public SupplierDoesNotExistException(String msg){
+        super(msg);
     }
 }
