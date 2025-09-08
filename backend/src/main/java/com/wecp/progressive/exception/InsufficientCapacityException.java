0f96f363d0bc0c7fb5d3860eb91f1f0a0e5e7776
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-08 09:52:27 +0530

    Day 9.java

--- backend/src/main/java/com/wecp/progressive/exception/InsufficientCapacityException.java
+++ backend/src/main/java/com/wecp/progressive/exception/InsufficientCapacityException.java
@@ -1,7 +1,7 @@
 package com.wecp.progressive.exception;
 
 public class InsufficientCapacityException extends RuntimeException {
-    public InsufficientCapacityException(String message) {
-        super(message);
+    public InsufficientCapacityException(String msg){
+        super(msg);
     }
 }
