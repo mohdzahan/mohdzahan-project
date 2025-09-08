6c5b2ffaee61fb624113a7288bf129720cf50666
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-08 10:18:40 +0530

    Day 9.java

--- backend/src/main/java/com/wecp/progressive/controller/WarehouseController.java
+++ backend/src/main/java/com/wecp/progressive/controller/WarehouseController.java
@@ -3,7 +3,6 @@ package com.wecp.progressive.controller;
 import com.wecp.progressive.entity.Warehouse;
 import com.wecp.progressive.exception.NoWarehouseFoundForSupplierException;
 import com.wecp.progressive.service.impl.WarehouseServiceImplJpa;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
