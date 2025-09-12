ec7318b114701545c1189021ba2e5e21403a5a2f
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-12 09:08:32 +0530

    Day 15.java

--- backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
@@ -1,9 +1,7 @@
 package com.wecp.progressive.repository;
 
 import com.wecp.progressive.entity.Supplier;
-
 import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
