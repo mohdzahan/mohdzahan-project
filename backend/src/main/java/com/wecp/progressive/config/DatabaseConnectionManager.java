2856e26421f94f26f42aba83563003d2ca315c68
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-20 22:57:43 +0530

    Day 25.java

--- backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
+++ backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
@@ -6,16 +6,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+
 public class DatabaseConnectionManager {
     private static final Properties properties = new Properties();
+
     static {
-        
         loadProperties();
     }
+
     private static void loadProperties() {
-try (InputStream input = DatabaseConnectionManager.class.getClassLoader()
-.getResourceAsStream("application.properties")) 
-{
+        try (InputStream input = DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("application.properties")) {
             if (input == null) {
                 throw new IllegalStateException("resource.properties not found in classpath");
             }
@@ -24,10 +24,11 @@ try (InputStream input = DatabaseConnectionManager.class.getClassLoader()
             throw new RuntimeException("Error loading properties file", e);
         }
     }
+
     public static Connection getConnection() throws SQLException {
         String url = properties.getProperty("spring.datasource.url");
         String user = properties.getProperty("spring.datasource.username");
-     String password = properties.getProperty("spring.datasource.password");
+        String password = properties.getProperty("spring.datasource.password");
         return DriverManager.getConnection(url, user, password);
     }
 }
