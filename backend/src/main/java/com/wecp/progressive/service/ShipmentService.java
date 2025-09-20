2eb0667df1ca94e3bdc95a247189752a50a547a3
Mohammed Zahan <91714136+mohdzahan@users.noreply.github.com>
2025-09-20 22:36:47 +0530

    Day 25.java

--- backend/src/main/java/com/wecp/progressive/service/ShipmentService.java
+++ backend/src/main/java/com/wecp/progressive/service/ShipmentService.java
@@ -4,13 +4,12 @@ import com.wecp.progressive.entity.Shipment;
 
 import java.sql.SQLException;
 import java.util.List;
-
 public interface ShipmentService {
     List<Shipment> getAllShipments() throws SQLException;
 
     Shipment getShipmentById(int shipmentId) throws SQLException;
 
-    int addShipment(Shipment shipment)throws SQLException;
+    int addShipment(Shipment shipment) throws SQLException;
 
     void updateShipment(Shipment shipment) throws SQLException;
 
